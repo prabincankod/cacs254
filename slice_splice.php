3add64afd58279b4cf6794ac5aa745e707ff9d90
Prabin Subedi <42871240+prabincankod@users.noreply.github.com>
2025-08-29 12:17:03 +0545

    feat: slice and splice

---
+++ slice_splice.php
@@ -0,0 +1,28 @@
+<?php
+$arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+
+$new_arr = array_slice($arr, 1, 2);
+
+
+// var_dump($arr);
+// var_dump($new_arr);
+
+
+
+// removing some , adding none
+array_splice($arr, 1, 3);
+// var_dump($arr); // this could be verbose
+print_r($arr);
+
+
+// removing none , adding some
+array_splice($arr, 1, 0, ['ram', 'shyam', 'hari']);
+// var_dump($arr); // this could be verbose
+print_r($arr);
+
+
+// removing some , adding some
+array_splice($arr, 0, 5, ['ram', 'shyam', 'hari']);
+// var_dump($arr); // this could be verbose
+print_r($arr);
