03caddaeefa48caaa148c7ee4436e22d4b691674
Prabin Subedi <42871240+prabincankod@users.noreply.github.com>
2025-09-07 12:25:01 +0545

    feat: more string related functions.

--- strings/intro.php
+++ strings/intro.php
@@ -28,10 +28,47 @@ $fruit = "dragon apple";
 $char = "q";
 $pos = strpos($fruit, $char);
 
-if($pos){
-    echo "exisit"; 
-}else{
-    echo " doesnot exist";
-}
+// if($pos){
+//     echo "exisit"; 
+// }else{
+//     echo " doesnot exist";
+// }
 
 // echo  "The character $char is at index : $pos";
+
+
+// reverse of a string 
+$str = "orange";
+$str1 = strrev($str);
+
+// echo "$str and $str1 ";
+
+
+// palindrome of a string
+
+$pal = "racecar";
+$pal_rev = strrev($pal);
+
+if (strcmp($pal, $pal_rev) == 0) {
+    // echo " $pal is palindrome ";
+} else {
+    // echo " $pal is not palindrome";
+}
+
+
+
+
+
+
+// implode / explode
+$users = "ram shyam sita geeta";
+// print_r(explode(' ', $users));
+
+
+// $users = "ram, shyam , sita, geeta";
+// print_r(explode(',', $users));
+
+
+$u_array = explode(" ", $users);
+
+print_r(implode(" ", $u_array));
